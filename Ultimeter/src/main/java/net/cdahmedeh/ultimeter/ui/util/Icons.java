735aef5202a2a749f4d4c89e75460720fc748b89
Ahmed El-Hajjar <me@cdahmedeh.net>
2015-06-20 17:49:50 -0400

    Make application packageable with Gradle 'application' plugin. Includes fix for loading icons when loading program from .jar.

--- Ultimeter/src/main/java/net/cdahmedeh/ultimeter/ui/util/Icons.java
+++ Ultimeter/src/main/java/net/cdahmedeh/ultimeter/ui/util/Icons.java
@@ -1,5 +1,7 @@
 package net.cdahmedeh.ultimeter.ui.util;
 
+import java.io.IOException;
+
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Widget;
 
@@ -11,11 +13,18 @@ public class Icons {
      * Retrieves a SWT Image from the icon set found in the resources/icons 
      * folder.
      * 
+     * It will return a blank 16x16 image if the file does not exist or cannot
+     * be loaded.
+     * 
      * @param widget A reference to a widget to get the Display from. 
      * @param iconName The name of the icon without the file extension or folder.
      * @return An Image reference to the created image.
      */
     public static Image getIcon(Widget widget, String iconName) {
-        return new Image(widget.getDisplay(), Resources.getResource("icons/" + iconName + ".png").getFile());
+        try {
+            return new Image(widget.getDisplay(), Resources.getResource("icons/" + iconName + ".png").openStream());
+        } catch (IOException e) {
+            return new Image(widget.getDisplay(), 16, 16);
+        }
     }
 }
